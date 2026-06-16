d768c89e797931550e2a5a836092cbb5d4bbf154
Nick Aversano <nick23hi@gmail.com>
2026-06-16 12:03:39 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # hello_emscripten
+
+## Setup
+
+First, install `emcc` and add it to your path: https://emscripten.org/docs/getting_started/downloads.html
+
+## Build
+
+Run:
+
+```sh
+./build.sh
+```
+
+Then open:
+
+- http://localhost:8080/sdl.html
+- http://localhost:8080/sdl2_webgpu.html
+- http://localhost:8080/webgl.html
+- http://localhost:8080/webgpu.html
+
--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 set -e
 
 project_root="$(cd "$(dirname "$0")" && pwd -P)"
+time=
+[ -f ~/bin/ntime ] && time=~/bin/ntime
 
 pushd $project_root
     mkdir -p build
@@ -9,12 +11,18 @@ pushd $project_root
     # can also use -O3
     # and --closure=1 for optimizing the JS
 
-    ntime emcc hello_sdl.c -o build/hello.html --shell-file minimal_shell.html -s ENVIRONMENT=web -s ASSERTIONS=0 -O0
+    $time emcc hello_sdl.c -o build/sdl.html --shell-file minimal_shell.html -s ENVIRONMENT=web -s ASSERTIONS=0 -O0
 
-    ntime emcc hello_webgl.c -o build/webgl.html -s USE_SDL=2 -s MIN_WEBGL_VERSION=2 --shell-file minimal_shell.html -s ENVIRONMENT=web -s ASSERTIONS=0
+    $time emcc hello_sdl2_webgl.c -o build/sdl2_webgl.html -s USE_SDL=2 -s MIN_WEBGL_VERSION=2 --shell-file minimal_shell.html -s ENVIRONMENT=web -s ASSERTIONS=0
 
-    ntime emcc hello_webgpu.c -o build/webgpu.html --shell-file minimal_shell.html --use-port=emdawnwebgpu -s ENVIRONMENT=web -s ASSERTIONS=0 -O0
+    $time emcc hello_webgl.c -o build/webgl.html --shell-file minimal_shell.html --use-port=emdawnwebgpu -s ENVIRONMENT=web -s ASSERTIONS=0 -O0
 
-    (sleep 0.1 && open http://localhost:8080/webgpu.html) &
+    $time emcc hello_webgpu.c -o build/webgpu.html --shell-file minimal_shell.html --use-port=emdawnwebgpu -s ENVIRONMENT=web -s ASSERTIONS=0 -O0
+
+    (sleep 0.1 && open http://localhost:8080/sdl.html) &
+    # (sleep 0.1 && open http://localhost:8080/sdl2_webgl.html) &
+    # (sleep 0.1 && open http://localhost:8080/webgl.html) &
+    # (sleep 0.1 && open http://localhost:8080/webgpu.html) &
+    
     python -m http.server 8080 --directory build
 popd
